69f3c31376e24c6390a5b024117ea9ba89472f9c
Rodrigo Trespalacios <rodrigo.trespalacios@gmail.com>
2017-06-22 16:24:34 +0200

    fasf

--- app/app.py
+++ app/app.py
@@ -28,6 +28,12 @@ def hello():
 #     ) = gitHttpBackend.wsgi_to_git_http_backend(environ, 'repos/git/test')
 #     return Response(response_body_generator, status_line, headers)
     # return send_from_directory('repos/git/test/', path)
+def add_headers(res, content_type):
+    res.headers['Expires'] = 'Fri, 01 Jan 1980 00:00:00 GMT'
+    res.headers['Pragma'] = 'no-cache'
+    res.headers['Cache-Control'] = 'no-cache, max-age=0, must-revalidate'
+    res.headers['Content-Type'] = content_type
+    return res
 
 def parse_file_tree(repo, tree):
     temp_dict = {}
@@ -36,8 +42,13 @@ def parse_file_tree(repo, tree):
         if e.type == 'tree':
             temp_dict[e.name] = parse_file_tree(repo, repo.get(e.id))
         else:
-            temp_dict[e.name] = e.id
+            temp_dict[e.name] = {"oid": e.id, "type": e.type}
     return temp_dict
+
+@app.route("/user/<string:project_name>/<string:oid>")
+def show_file(project_name, oid):
+    return render_template("file.html", oid=oid)
+
 
 @app.route("/user/<string:project_name>")
 def list_files(project_name):
@@ -45,9 +56,9 @@ def list_files(project_name):
     tree = repo.revparse_single('master').tree
     tree_dict = {}
     tree_dict = parse_file_tree(repo, tree)
-    # file_list = [e for e in tree]
-    return render_template("filetree.html", file_list=tree_dict)
-    # return jsonify(file_list)
+    print(tree_dict)
+    return render_template("filetree.html", file_list=tree_dict.items(), base_url='/{}/{}'.format('user', project_name))
+
 
 @app.route('/<string:project_name>/info/refs')
 def info_refs(project_name):
@@ -61,34 +72,25 @@ def info_refs(project_name):
     data = str.encode(prefix) + str.encode(packet) + b'0000'
     data += p.stdout.read()
     res = make_response(data)
-    res.headers['Expires'] = 'Fri, 01 Jan 1980 00:00:00 GMT'
-    res.headers['Pragma'] = 'no-cache'
-    res.headers['Cache-Control'] = 'no-cache, max-age=0, must-revalidate'
-    res.headers['Content-Type'] = 'application/x-%s-advertisement' % service
     p.wait()
-    return res
+    return add_headers(res, 'application/x-%s-advertisement' % service)
+
 
 @app.route('/<string:project_name>/git-receive-pack', methods=('POST',))
 def git_receive_pack(project_name):
     p = subprocess.Popen(['git-receive-pack', '--stateless-rpc', os.path.join('.', project_name)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     data = p.communicate(input=request.data)[0]
     res = make_response(data)
-    res.headers['Expires'] = 'Fri, 01 Jan 1980 00:00:00 GMT'
-    res.headers['Pragma'] = 'no-cache'
-    res.headers['Cache-Control'] = 'no-cache, max-age=0, must-revalidate'
-    res.headers['Content-Type'] = 'application/x-git-receive-pack-result'
-    return res
+    return add_headers(res, 'application/x-git-receive-pack-result')
+
 
 @app.route('/<string:project_name>/git-upload-pack', methods=('POST',))
 def git_upload_pack(project_name):
     p = subprocess.Popen(['git-upload-pack', '--stateless-rpc', os.path.join('.', project_name)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     data = p.communicate(input=request.data)[0]
     res = make_response(data)
-    res.headers['Expires'] = 'Fri, 01 Jan 1980 00:00:00 GMT'
-    res.headers['Pragma'] = 'no-cache'
-    res.headers['Cache-Control'] = 'no-cache, max-age=0, must-revalidate'
-    res.headers['Content-Type'] = 'application/x-git-upload-pack-result'
-    return res
+    return add_headers(res, 'application/x-git-upload-pack-result')
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
