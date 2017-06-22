9e48bfe66466767d8625870fdaea277c1919342e
Rodrigo Trespalacios <rodrigo.trespalacios@gmail.com>
2017-06-22 16:52:12 +0200

    file-viewer

--- app/app.py
+++ app/app.py
@@ -6,28 +6,7 @@ import sys
 import pygit2
 import binascii
 
-@app.route("/")
-def hello():
-    return render_template("index.html")
 
-@app.route("/create/<string:user>/<string:project_name>")
-def create(user, project_name):
-    pygit2.init_repository(os.path.join("./repos", project_name), True)
-    return "Create!"
-
-# @app.route("/clone/info/<path:path>")
-# def clone(path):
-#     return 'h'
-
-# @app.route("/clone/<path:path>")
-# def cloneHead(path):
-#     environ = dict(request.environ)
-#     environ['PATH_INFO'] = environ['PATH_INFO'][4:]
-#     (
-#         status_line, headers, response_body_generator
-#     ) = gitHttpBackend.wsgi_to_git_http_backend(environ, 'repos/git/test')
-#     return Response(response_body_generator, status_line, headers)
-    # return send_from_directory('repos/git/test/', path)
 def add_headers(res, content_type):
     res.headers['Expires'] = 'Fri, 01 Jan 1980 00:00:00 GMT'
     res.headers['Pragma'] = 'no-cache'
@@ -42,22 +21,33 @@ def parse_file_tree(repo, tree):
         if e.type == 'tree':
             temp_dict[e.name] = parse_file_tree(repo, repo.get(e.id))
         else:
-            temp_dict[e.name] = {"oid": e.id, "type": e.type}
+            temp_dict[e.name] = {'oid': e.id, 'type': e.type}
     return temp_dict
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/create/<string:user>/<string:project_name>")
+def create(user, project_name):
+    pygit2.init_repository(os.path.join("./repos", project_name), True)
+    return "Create!"
 
 @app.route("/user/<string:project_name>/<string:oid>")
 def show_file(project_name, oid):
-    return render_template("file.html", oid=oid)
+    repo = pygit2.Repository(os.path.join('./repos', project_name))
+    blob = repo.get(oid)
+    return render_template('file.html', oid=oid, file=blob.data)
 
 
 @app.route("/user/<string:project_name>")
 def list_files(project_name):
-    repo = pygit2.Repository(project_name)
+    repo = pygit2.Repository(os.path.join("./repos", project_name))
     tree = repo.revparse_single('master').tree
     tree_dict = {}
     tree_dict = parse_file_tree(repo, tree)
     print(tree_dict)
-    return render_template("filetree.html", file_list=tree_dict.items(), base_url='/{}/{}'.format('user', project_name))
+    return render_template('filetree.html', file_list=tree_dict.items(), base_url='/{}/{}'.format('user', project_name))
 
 
 @app.route('/<string:project_name>/info/refs')
@@ -65,7 +55,7 @@ def info_refs(project_name):
     service = request.args.get('service')
     if service[:4] != 'git-':
         abort(500)
-    p = subprocess.Popen([service, '--stateless-rpc', '--advertise-refs', os.path.join('.', project_name)], stdout=subprocess.PIPE)
+    p = subprocess.Popen([service, '--stateless-rpc', '--advertise-refs', os.path.join('./repos', project_name)], stdout=subprocess.PIPE)
     packet = '# service=%s\n' % service
     length = len(packet) + 4
     prefix = "{:04x}".format(length & 0xFFFF);
@@ -78,7 +68,7 @@ def info_refs(project_name):
 
 @app.route('/<string:project_name>/git-receive-pack', methods=('POST',))
 def git_receive_pack(project_name):
-    p = subprocess.Popen(['git-receive-pack', '--stateless-rpc', os.path.join('.', project_name)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    p = subprocess.Popen(['git-receive-pack', '--stateless-rpc', os.path.join('./repos', project_name)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     data = p.communicate(input=request.data)[0]
     res = make_response(data)
     return add_headers(res, 'application/x-git-receive-pack-result')
@@ -86,7 +76,7 @@ def git_receive_pack(project_name):
 
 @app.route('/<string:project_name>/git-upload-pack', methods=('POST',))
 def git_upload_pack(project_name):
-    p = subprocess.Popen(['git-upload-pack', '--stateless-rpc', os.path.join('.', project_name)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    p = subprocess.Popen(['git-upload-pack', '--stateless-rpc', os.path.join('./repos', project_name)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     data = p.communicate(input=request.data)[0]
     res = make_response(data)
     return add_headers(res, 'application/x-git-upload-pack-result')
