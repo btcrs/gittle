f411c8f7d048d2d1734187e0e840887428e0cdc3
ben <benjamin.carothers13@ncf.edu>
2017-06-22 15:01:20 +0200

    Opens up port 80

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ docker stop ${CONTAINER_NAME}
 docker rm ${CONTAINER_NAME}
 
 docker build -t ${CONTAINER_NAME} .
-docker run --rm -it --name=${CONTAINER_NAME} -p 5000:5000 -v `pwd`/app:/code -w /code ${CONTAINER_NAME} python app.py
+docker run --rm -d --name=${CONTAINER_NAME} -p 80:5000 -v `pwd`/app:/code -w /code ${CONTAINER_NAME} python app.py
