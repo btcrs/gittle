1c6a2af3dcc26e9c7bdfb70389836fda8dafa35d
Rodrigo <rodrigo@renthunt.co>
2017-07-27 17:32:57 +0200

    Prints

--- groot/versions/views.py
+++ groot/versions/views.py
@@ -323,6 +323,8 @@ def read_tree(request, user, project_name, permissions_token):
     try:
         path = request.GET.get('path').rstrip('/').lstrip('/')
         directory = porcelain.generate_directory(user)
+        print(directory)
+        print(project_name)
         repo = pygit2.Repository(os.path.join('./repos', directory, project_name))
         git_tree, git_blob = porcelain.walk_tree(repo, path)
         parsed_tree = None
